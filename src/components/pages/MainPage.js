926da579bf27523886c01b20d6068cb8c731360e
soominnn <qkrtnals970804@gmail.com>
2022-11-09 16:45:43 +0900

    [221109] feat: 실시간 동영상 종료 시 결과 출력 기능

--- src/components/pages/MainPage.js
+++ src/components/pages/MainPage.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, {useState} from "react";
 import { styled } from '@mui/material/styles';
 import Box from '@mui/material/Box';
 import Paper from '@mui/material/Paper';
@@ -21,12 +21,14 @@ const Item = styled(Paper)(({ theme }) => ({
 
 function MainPage(){
     const userID = useRecoilValue(userId);
+    const [turnOffVideo, setTurnOffVideo] = useState(false);
     console.log(userID)
     //동영상 녹화 시작
     const videoStart = async () => {
         await axios
             .get(`http://localhost:8080/python/${userID}`)
             .then((res) => {
+                setTurnOffVideo(true);
                 console.log(res);
             })
             .catch((err) => {
@@ -50,13 +52,17 @@ function MainPage(){
                     <Grid xs={8}>
                         {/*오른편에 동영상 추천 및 비교 글 넣기*/}
                         <Item>
-                            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Sit amet venenatis urna cursus eget nunc scelerisque viverra. Pulvinar sapien et ligula ullamcorper malesuada. Semper feugiat nibh sed pulvinar proin gravida. Ipsum nunc aliquet bibendum enim facilisis gravida neque. Volutpat diam ut venenatis tellus in metus vulputate eu. Fusce ut placerat orci nulla pellentesque. Fermentum posuere urna nec tincidunt praesent semper feugiat. Nulla pellentesque dignissim enim sit amet venenatis urna cursus. Et leo duis ut diam quam nulla.
+                        { turnOffVideo &&
+                            <div>
+                                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Sit amet venenatis urna cursus eget nunc scelerisque viverra. Pulvinar sapien et ligula ullamcorper malesuada. Semper feugiat nibh sed pulvinar proin gravida. Ipsum nunc aliquet bibendum enim facilisis gravida neque. Volutpat diam ut venenatis tellus in metus vulputate eu. Fusce ut placerat orci nulla pellentesque. Fermentum posuere urna nec tincidunt praesent semper feugiat. Nulla pellentesque dignissim enim sit amet venenatis urna cursus. Et leo duis ut diam quam nulla.
 
-                            Scelerisque eleifend donec pretium vulputate. Laoreet non curabitur gravida arcu. Lectus arcu bibendum at varius vel pharetra. Nibh sed pulvinar proin gravida hendrerit. Quam quisque id diam vel quam elementum. Laoreet id donec ultrices tincidunt arcu non sodales neque sodales. Nullam vehicula ipsum a arcu cursus vitae congue mauris rhoncus. Pellentesque sit amet porttitor eget. Elit pellentesque habitant morbi tristique senectus. Nisi vitae suscipit tellus mauris a diam maecenas.
+                                Scelerisque eleifend donec pretium vulputate. Laoreet non curabitur gravida arcu. Lectus arcu bibendum at varius vel pharetra. Nibh sed pulvinar proin gravida hendrerit. Quam quisque id diam vel quam elementum. Laoreet id donec ultrices tincidunt arcu non sodales neque sodales. Nullam vehicula ipsum a arcu cursus vitae congue mauris rhoncus. Pellentesque sit amet porttitor eget. Elit pellentesque habitant morbi tristique senectus. Nisi vitae suscipit tellus mauris a diam maecenas.
 
-                            Commodo sed egestas egestas fringilla phasellus faucibus. Semper risus in hendrerit gravida rutrum quisque non tellus. Ac odio tempor orci dapibus ultrices in iaculis. Bibendum neque egestas congue quisque egestas diam. Id ornare arcu odio ut sem nulla pharetra diam. Sed augue lacus viverra vitae congue eu consequat ac felis. Egestas pretium aenean pharetra magna. Mi bibendum neque egestas congue. Ac turpis egestas maecenas pharetra convallis posuere morbi. Congue nisi vitae suscipit tellus mauris a. Nisl tincidunt eget nullam non nisi. At imperdiet dui accumsan sit amet nulla facilisi. Suspendisse in est ante in nibh mauris cursus mattis. Lorem ipsum dolor sit amet consectetur adipiscing elit pellentesque. Urna neque viverra justo nec ultrices. Tincidunt eget nullam non nisi est sit amet. Scelerisque mauris pellentesque pulvinar pellentesque habitant morbi tristique senectus et. Enim nunc faucibus a pellentesque.
+                                Commodo sed egestas egestas fringilla phasellus faucibus. Semper risus in hendrerit gravida rutrum quisque non tellus. Ac odio tempor orci dapibus ultrices in iaculis. Bibendum neque egestas congue quisque egestas diam. Id ornare arcu odio ut sem nulla pharetra diam. Sed augue lacus viverra vitae congue eu consequat ac felis. Egestas pretium aenean pharetra magna. Mi bibendum neque egestas congue. Ac turpis egestas maecenas pharetra convallis posuere morbi. Congue nisi vitae suscipit tellus mauris a. Nisl tincidunt eget nullam non nisi. At imperdiet dui accumsan sit amet nulla facilisi. Suspendisse in est ante in nibh mauris cursus mattis. Lorem ipsum dolor sit amet consectetur adipiscing elit pellentesque. Urna neque viverra justo nec ultrices. Tincidunt eget nullam non nisi est sit amet. Scelerisque mauris pellentesque pulvinar pellentesque habitant morbi tristique senectus et. Enim nunc faucibus a pellentesque.
 
-                            Quam elementum pulvinar etiam non quam. Duis convallis convallis tellus id interdum velit laoreet id. Nisi quis eleifend quam adipiscing vitae. Consectetur adipiscing elit duis tristique sollicitudin nibh sit amet. Interdum posuere lorem ipsum dolor sit amet. Fusce id velit ut tortor pretium viverra suspendisse potenti. Volutpat commodo sed egestas egestas fringilla phasellus. Vivamus arcu felis bibendum ut tristique et. Ac placerat vestibulum lectus mauris ultrices. Sit amet cursus sit amet dictum sit amet justo. Auctor neque vitae tempus quam pellentesque. Tempor commodo ullamcorper a lacus vestibulum sed arcu non odio. Nec ultrices dui sapien eget mi. Sit amet cursus sit amet dictum sit amet. Id volutpat lacus laoreet non curabitur gravida. Velit sed ullamcorper morbi tincidunt ornare massa eget egestas. Leo vel orci porta non pulvinar neque laoreet suspendisse interdum. Volutpat est velit egestas dui id ornare arcu. Eu volutpat odio facilisis mauris sit amet massa vitae tortor.
+                                Quam elementum pulvinar etiam non quam. Duis convallis convallis tellus id interdum velit laoreet id. Nisi quis eleifend quam adipiscing vitae. Consectetur adi
+                            </div>
+                        }
                         </Item>
                     </Grid>
                 </Grid>
